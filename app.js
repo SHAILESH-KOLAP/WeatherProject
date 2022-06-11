89fd7394ede1372496dab283af3a711eae72c032
Shailesh Kolap <skolap22@gmail.com>
2022-06-11 16:23:43 +0530

    Get API data from Open Weather Map using https module

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const https = require("https");
 const app = express();
+
+app.get("/", function (req, res) {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=fe040a917fb3fc71f6538dc8f6d23ead&units=metric";
+  https.get(url, function (response) {
+    console.log(response);
+  });
+});
 
 app.listen(3000, function () {
   console.log("Listening on port 3000");
